b1f2b33bfa3c3d7ab53b54042d0ae227badba094
VincentChisaka <vincentchisaka2@gmail.com>
2023-02-10 21:57:52 +0300

    done lab

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+     self[-1] == "?"
   end
 
   def exclamation?
-
+     self[-1] == "!"
   end
 
+  # lastly the count method returns the number of elems in the array
   def count_sentences
+    self.split(/[.?!]+/).reject(&:empty?).count
 
   end
 end
+
+binding.pry
